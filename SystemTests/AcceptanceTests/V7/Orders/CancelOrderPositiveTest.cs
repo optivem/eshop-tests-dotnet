ccee54506398349b4e508a82ca2d61130ba46011
Valentina Jemuović <valentina.jemuovic@optivem.com>
2026-02-09 14:46:43 +0100

    refactor: Remove TODO skips from CancelOrder tests to enable execution and improve test coverage

--- SystemTests/AcceptanceTests/V7/Orders/CancelOrderPositiveTest.cs
+++ SystemTests/AcceptanceTests/V7/Orders/CancelOrderPositiveTest.cs
@@ -7,7 +7,7 @@ namespace Optivem.EShop.SystemTest.AcceptanceTests.V7.Orders;
 
 public class CancelOrderPositiveTest : BaseAcceptanceTest
 {
-    [Theory(Skip = "TODO: FIX")]
+    [Theory]
     [Time]
     [ChannelData(ChannelType.UI, ChannelType.API)]
     [ChannelInlineData("2024-12-31T21:59:59Z")]   // 1 second before blackout period starts
