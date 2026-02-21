475f8c5a82448d36f79f8cab42f12e186aab50d0
Valentina Jemuović <valentina.jemuovic@optivem.com>
2026-02-21 18:38:23 +0100

    Refactor: Update time in BaseClockContractTest for consistency in test scenarios

--- SystemTests/ExternalSystemContractTests/V7/Clock/BaseClockContractTest.cs
+++ SystemTests/ExternalSystemContractTests/V7/Clock/BaseClockContractTest.cs
@@ -8,7 +8,7 @@ public abstract class BaseClockContractTest : BaseExternalSystemContractTest
     public async Task ShouldBeAbleToGetTime()
     {
         (await App.Clock().ReturnsTime()
-            .Time("2024-06-15T12:00:00Z")
+            .Time("2024-01-02T09:00:00Z")
             .Execute())
             .ShouldSucceed();
 
