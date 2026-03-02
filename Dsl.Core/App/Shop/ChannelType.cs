19886a6d4e3db9666b033a7ad35712534c6102e2
Valentina Jemuović <valentina.jemuovic@optivem.com>
2026-03-02 15:07:58 +0100

    feat: Add Channel module with ChannelType definitions and update project references

--- Dsl.Core/App/Shop/ChannelType.cs
+++ Dsl.Core/App/Shop/ChannelType.cs
@@ -2,7 +2,6 @@ namespace Dsl.Core.Shop;
 
 public static class ChannelType
 {
-    public const string UI = "UI";
-    public const string API = "API";
+    public const string UI = global::Optivem.EShop.SystemTest.Channel.ChannelType.UI;
+    public const string API = global::Optivem.EShop.SystemTest.Channel.ChannelType.API;
 }
-
