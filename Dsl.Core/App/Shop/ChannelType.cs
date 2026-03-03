a68d2f973cc903bbfb29042782cd54809b088309
Valentina Jemuović <valentina.jemuovic@optivem.com>
2026-03-03 10:28:36 +0100

    feat: Remove ChannelType class and update global usings across test projects

--- Dsl.Core/App/Shop/ChannelType.cs
+++
@@ -1,7 +0,0 @@
-namespace Dsl.Core.Shop;
-
-public static class ChannelType
-{
-    public const string UI = global::Optivem.EShop.SystemTest.Channel.ChannelType.UI;
-    public const string API = global::Optivem.EShop.SystemTest.Channel.ChannelType.API;
-}
--- SystemTests/SmokeTests/GlobalUsings.cs
+++ SystemTests/SmokeTests/GlobalUsings.cs
@@ -1,1 +1,2 @@
 global using Xunit;
+global using Optivem.EShop.SystemTest.Channel;
