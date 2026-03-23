c55728f758882aba6b43cd4b0b891dbad33da0bc
Valentina Jemuović <valentina.jemuovic@optivem.com>
2026-03-23 15:27:33 +0100

    Updated

--- SystemTests/GlobalUsings.cs
+++ SystemTests/GlobalUsings.cs
@@ -3,7 +3,6 @@ global using Optivem.EShop.SystemTest.Channel;
 global using Optivem.Testing;
 global using Dsl.Core.Shared;
 global using SystemTests.TestInfrastructure.Configuration;
-global using SystemTests.V8.ExternalSystemContractTests.Base;
 
 
 
